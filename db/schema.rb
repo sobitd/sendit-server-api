2c1490a1f0e254291baad80c667de1ae774d7de1
sobitd <sobitdaniel@gmail.com>
2022-11-01 13:42:03 +0300

    seed to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_01_102353) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_01_104002) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_01_102353) do
     t.integer "distance"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_parcels_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,4 +41,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_01_102353) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "parcels", "users"
 end
