96196c550d2fb6b3d2495e44cb3e36fb11e20a0b
sobitd <sobitdaniel@gmail.com>
2022-11-03 01:21:43 +0300

    add authorization rails

--- app/controllers/parcels_controller.rb
+++ app/controllers/parcels_controller.rb
@@ -1,47 +1,55 @@
 class ParcelsController < ApplicationController
+  before_action :common_setup, only: %i[show update destroy]
+  before_action :authorized
   # GET /parcels
   def index
-    parcels = Parcel.all
-    render json: parcels
+    @parcels = Parcel.where(user_id: @user.id)
+    render json: @parcels
   end
 
   # GET /parcels/1
   def show
-    parcel = Parcel.find(params[:id])
-    render json: parcel
+    # parcel = Parcel.find(params[:id])
+    render json: @parcel
   end
 
   # POST /parcels
   def create
-    parcel = Parcel.new(parcel_params)
+    @parcel = Parcel.new(parcel_params)
+    @parcel.user_id = @user.id
 
-    if parcel.save
-      render json: parcel, status: :created
+    if @parcel.save
+      render json: @parcel, status: :created, location: @parcel
     else
-      render json: parcel.errors, status: :unprocessable_entity
+      render json: @parcel.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /parcels/1
   def update
-    if parcel.update(parcel_params)
-      render json: parcel 
-    else  
-      render json: parcel.errors, status: :unprocessable_entity
+    if @parcel.update(parcel_params)
+      render json: @parcel
+    else
+      render json: @parcel.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /parcels/1
   def destroy
-    Parcel.destroy(params[:id])
+    @parcel.destroy
   end
 
   private
 
+  # Use callbacks to share common setup or constraints between actions
+  def common_setup
+    @parcel = Parcel.find(params[:id])
+  end
+
   # Use callbacks to share common setup or constraints between actions.
   # Only allow a list of trusted parameters through.
   def parcel_params
-    params.permit(:recipient_name, :recipient_contact, :weight, :from, :destination, :order_status,
-                  :distance, :user_id)
+    params.require(:parcel).permit(:recipient_name, :recipient_contact, :weight, :from, :destination, :order_status,
+                                   :distance, :user_id)
   end
 end
