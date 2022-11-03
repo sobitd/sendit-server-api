0b2e367e551eda075cd5ead1226fee278fec4043
sobitd <sobitdaniel@gmail.com>
2022-11-03 10:46:58 +0300

    change isAdmin datatype to integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_01_193811) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_03_074400) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,9 +36,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_01_193811) do
     t.string "username"
     t.string "password_digest"
     t.string "email_address"
-    t.boolean "isAdmin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
+    t.string "encrypted_password", limit: 128
+    t.string "confirmation_token", limit: 128
+    t.string "remember_token", limit: 128
+    t.integer "isAdmin"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["remember_token"], name: "index_users_on_remember_token", unique: true
   end
 
   add_foreign_key "parcels", "users"
